407208536cf378296379342100d0620fe8ee2f4e
Wiranto Aries M <kiwiranto@gmail.com>
2019-05-09 16:52:47 +0700

    Updates

---
+++ precache-manifest.121fba89e8ada45a0d9d7a4e39413c7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2615b2f48399bfc1be4710576b069098",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "9a26fdcd00b9b48ee359",
+    "url": "/react-portfolio/static/css/main.f312b235.chunk.css"
+  },
+  {
+    "revision": "0679f1effd312b8c6c3b",
+    "url": "/react-portfolio/static/js/2.8c6a0030.chunk.js"
+  },
+  {
+    "revision": "9a26fdcd00b9b48ee359",
+    "url": "/react-portfolio/static/js/main.47a50160.chunk.js"
+  },
+  {
+    "revision": "94872cf7e81768d148a3",
+    "url": "/react-portfolio/static/js/runtime~main.f974a8eb.js"
+  }
+]);
